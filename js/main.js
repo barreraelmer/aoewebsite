86bb8b813a668914c226e9b8a86629fac58dd3a7
liawesomesaucer <vincent.oddrepublic@gmail.com>
2016-03-09 19:56:14 -0800

    Try black barkcgorund

--- js/main.js
+++ js/main.js
@@ -41,27 +41,19 @@ function update_hours(d) {
 wrapper = $(".menu");
 sidebar = $(".menu-sidebar");
 navbar = $(".navbar");
-// +5 is a temporary fix to remove jerkiness (I don't know where I'm off)
-wrap_pad_bottom = parseInt($(".center-column").css("padding-bottom")) + 5;
-bottomed = false;
-
 $(window).scroll(() => {
-
-    sidebartop = $(window).scrollTop() + $(sidebar).height() + parseInt($(sidebar).css("top"));
-    wrappertop = $(wrapper).offset().top + $(wrapper).height() - wrap_pad_bottom;
 
     // Temporary cancellation of this feature on default /browse page, change this
     // if (window.location.href.match(/browse\/.+\?/)) {
         mt = $(".navbar").height();
         stop = $(wrapper).offset().top + $(wrapper).height() - $(sidebar).height() - parseInt($(sidebar).css("top")) - 10;
-        if ($(window).scrollTop() > stop) {
+        if ($(window).scrollTop() > stop) { // Bottomed
             console.log("yes");
             topp = $(wrapper).height() - $(sidebar).height() - parseInt($(wrapper).css("padding"));
             $(sidebar).css({"position": "static", "margin-top": topp});
         }
         else if ($(window).scrollTop() > $(".jumbotron-menu").height()) {
             $(sidebar).css({"position": "fixed", "top": mt, "margin-top": "10px"});
-            bottomed = false;
         }
         else {
             mt = ($(wrapper).height() - $(wrapper).offset().top - 2*$(sidebar).height()).toString() + "px";
@@ -70,12 +62,3 @@ $(window).scroll(() => {
         }
 });
 
-// function nav_update() {
-//     if ($(window).scrollTop() > 0) {
-//         $(".navbar-inverse").css({"background":"#fff"});
-//         $(".navlink").addClass("green").removeClass("white");
-//     } else {
-//         $(".navbar-inverse").css({"background":"transparent"});
-//         $(".navlink").removeClass("green").addClass("white");
-//     }
-// }
