74be48daef52a15b0961f285374b58c098848800
liawesomesaucer <vincent.oddrepublic@gmail.com>
2016-03-09 23:24:43 -0800

    mobile-friendlied food/drink site

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,6 +2,7 @@
 wrapper = $(".menu");
 sidebar = $(".menu-sidebar");
 navbar = $(".navbar");
+// HEEEECKK YEEEEAAAHAHHHHHHH
 $(window).scroll(() => {
     top_offset = 80;
     mt = ($(".navbar").height() + top_offset).toString() + "px";
@@ -17,5 +18,17 @@ $(window).scroll(() => {
     else {
         mt = ($(wrapper).height() - $(wrapper).offset().top - 2*$(sidebar).height()).toString() + "px";
         $(sidebar).css({"position": "static", "margin-top": "10px"});
+    }
+});
+
+$(".menu-link").click((event) => {
+    content = $(event.target).parent().nextAll(".menu-section-content");
+    if ($(event.target).hasClass("open")) {
+        content.css({"display": "none"});
+        $(event.target).removeClass("open");
+    }
+    else {
+        content.css({"display": "block"});
+        $(event.target).addClass("open");
     }
 });
