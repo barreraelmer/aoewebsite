c96cf8b4a2624d5371f806a224c24361bbd1c7df
liawesomesaucer <vincent.oddrepublic@gmail.com>
2016-03-10 02:59:38 -0800

    Working on mobile-friendliness

--- js/sidebar.js
+++ js/sidebar.js
@@ -7,12 +7,12 @@ $(window).scroll(() => {
     top_offset = 80;
     mt = ($(".navbar").height() + top_offset).toString() + "px";
     sideheight = $(".sidebar").height();
-    stop = $(wrapper).offset().top + $(wrapper).height() - $(sidebar).height() - parseInt($(sidebar).css("top"));
+    stop = $(wrapper).offset().top + $(wrapper).height() - $(sidebar).height() - parseInt($(sidebar).css("top")) - 30;
     if ($(window).scrollTop() > stop) { // Bottomed
-        topp = $(wrapper).height() - $(sidebar).height() - parseInt($(wrapper).css("padding"));
+        topp = $(wrapper).height() - $(sidebar).height() - parseInt($(wrapper).css("padding")) - 30;
         $(sidebar).css({"position": "static", "margin-top": topp});
     }
-    else if ($(window).scrollTop() + top_offset > $(".jumbotron-menu").height() + 10) {
+    else if ($(window).scrollTop() + top_offset > $(".jumbotron-menu").height() - 255) {
         $(sidebar).css({"position": "fixed", "top": mt, "margin-top": "0px"});
     }
     else {
